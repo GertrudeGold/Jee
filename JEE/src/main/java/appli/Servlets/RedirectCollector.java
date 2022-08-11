6a0c3c7cbe89c31d5d11f3e1b60752c59211eefe
ThomasDelhaye <thomas_delhaye@hotmail.com>
2022-08-11 23:21:12 +0200

    Fixing Modify amount of violation for collector

--- JEE/src/main/java/appli/Servlets/RedirectCollector.java
+++ JEE/src/main/java/appli/Servlets/RedirectCollector.java
@@ -1,11 +1,17 @@
 package appli.Servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import appli.Javabeans.Collector;
+import appli.Javabeans.Violation;
 
 @WebServlet("/RedirectCollector")
 public class RedirectCollector extends HttpServlet {
@@ -18,6 +24,8 @@ public class RedirectCollector extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		Collector connected = (Collector) session.getAttribute("ConnectedStaff");
 		//See Violation
 		if (request.getParameter("ManageViolationAmount") != null) {
 			response.sendRedirect("ListViolationCollector");
@@ -28,8 +36,13 @@ public class RedirectCollector extends HttpServlet {
         }
 		//Modify amount
 		if (request.getParameter("ModifyViolation") != null) {
-			
-			
+			String typeViolation = request.getParameter("typeViolation");
+			ArrayList<Violation> violations = connected.getViolations();
+			for(Violation violation : violations) {
+				if(violation.getType().equals(typeViolation)) {
+					request.setAttribute("violation", violation);
+				}	
+			}
 			request.getRequestDispatcher("/WEB-INF/JSP/ModifyViolationCollector.jsp").forward(request,response);
         }
 	}
